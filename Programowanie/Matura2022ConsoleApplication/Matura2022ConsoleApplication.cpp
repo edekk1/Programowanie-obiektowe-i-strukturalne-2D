7bd6f9a60d01309ca8a1f2a84f80a66a0b09f3a7
q <q>
2025-03-31 13:59:03 +0200

    31.03.25

--- Programowanie/Matura2022ConsoleApplication/Matura2022ConsoleApplication.cpp
+++ Programowanie/Matura2022ConsoleApplication/Matura2022ConsoleApplication.cpp
@@ -1,20 +1,40 @@
-// Matura2022ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+//Matura 2022 Informatyka Poziom Rozsze¿ony
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Zadanie pierwsze
+	setlocale(LC_CTYPE, "Polish");
+	std::ifstream file("przyklad.txt");
+	std::vector<int> numbers;
+	int number;
+
+	while (file >> number)
+	{
+		numbers.push_back(number);
+	}
+
+	int counter = 0;
+	int first = 0;
+	for (int number : numbers)
+	{
+		std::string str = std::to_string(number);
+
+		if (str.length() > 0)
+		{
+			if (str.front() == str.back())
+			{
+				if (first == 0)
+				{
+					first = std::stoi(str);
+				}
+				counter++;
+			}
+		}
+	}
+
+	std::cout << counter << " " << first << "\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
